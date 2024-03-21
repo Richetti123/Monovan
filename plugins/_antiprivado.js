79a6be2de1c683a9ff10b9189842df8170b37e39
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-03-21 23:11:06 +0100

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -25,7 +25,7 @@ console.log(`horaActualNum >${horaActualNum}<`); //Luego comentar
 console.log(`tiempoTranscurrido >${tiempoTranscurrido}<`); //Luego comentar
 
 if (tiempoTranscurrido >= intervaloEsperado) {
-   await m.reply(`*🐱Hola @${m.sender.split`@`[0]}, bienvenido al chatbot de MichiBot🐱*\nSoy el asistente virtual de Geri, y estare a cargo de atenderte hoy\n\nSi deseas ver la lista de productos y servicios que tengo a la venta escribe:\n*menu de ventas*`, false, {mentions: [m.sender]});
+   await m.reply(`*¡Hola, @${m.sender.split`@`[0]}¡Espero que estés genial! Soy Monovan y estoy aquí para satisfacer todas tus necesidades.*\n¿Quieres saber más sobre lo que puedo ofrecerte? Estoy listo para impresionarte. 💼\nPara poder ver todos los artículos que vendemos escribe menu de ventas🛍️`, false, {mentions: [m.sender]});
     user.fechaUltimoMsjInbox = horaActualNum;
     return;
 }
